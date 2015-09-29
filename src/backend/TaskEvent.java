a2d354b1603b150ec0d4b983f5609f86f0671be8
zhongwei <zhangzhongwei.sg@gmail.com>
2015-09-29 18:30:33 +0800

    basic constructor for TaskEvent with taskID

--- src/backend/TaskEvent.java
+++ src/backend/TaskEvent.java
@@ -1,7 +1,20 @@
 package backend;
 
 public class TaskEvent {
-	public TaskEvent() {
 
-	}
+    protected long taskID;
+    protected String taskName;
+    protected int priority;
+    protected String description;
+    protected boolean isRecurring
+    protected TaskDate date;
+
+    public TaskEvent(String name, TaskDate date, int prio, String des, boolean recur) {
+        this.taskID = System.currentTimeMillis();
+        this.name = name;
+        this.date = date;
+        this.priority = prio;
+        this.description = des;
+        this.isRecurring = recur;
+    }
 }
