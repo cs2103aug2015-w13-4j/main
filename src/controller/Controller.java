58714c340e86de4b32b8819eef36a4c5fb499ad3
shirlenequah <shirlenequah@yahoo.com.sg>
2015-09-29 17:10:06 +0800

    controller
    reupload

---
+++ src/controller/Controller.java
@@ -0,0 +1,50 @@
+package controller;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Object.TaskEvent;
+
+public class Controller {
+	private static Scanner scanner = new Scanner(System.in);
+	public static void welcome(){
+		File file = new File(getFileName());
+		ArrayList<TaskEvent> tasks = readFile(file);
+	}
+	public static String getFileName(){
+		System.out.println("Welcome to Pixelist. Enter your file name");
+		String file = scanner.next();
+		return file;
+	}
+	public static void command(){
+		int command = getCommandType();
+		if(command == 0){
+			add();
+		} else if(command == 1){
+			display();
+		}
+		else {
+			
+		}
+	}
+	public static void add(){
+		TaskEvent newTask = getContext();
+		
+		
+		defaultView();
+	}
+	public static void display(){
+		String word = getWord();
+		//all
+		if(word =="all"){
+			defaultView();
+		}
+		
+	}
+	
+	//to update the whole list in default
+	public static void defaultView(){
+		
+	}
+}
