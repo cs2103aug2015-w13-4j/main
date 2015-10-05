d16f3e7a657c9be8865bd01ae8c1dbc23ada3a14
shirlenequah <shirlenequah@yahoo.com.sg>
2015-10-05 13:21:02 +0800

    calling storage method

--- src/logic/Operation.java
+++ src/logic/Operation.java
@@ -1,6 +1,8 @@
 package logic;
 
 import java.util.ArrayList;
+
+import database.Storage;
 import utilities.CommandElements;
 import utilities.Command_Type;
 import parser.ProcessInput;
@@ -8,15 +10,22 @@ import parser.ProcessInput;
 public class Operation {
 	private String processOperation(String input){
 		//get the commandElements from parser
+		Display message;
 		CommandElements processed = ProcessInput(input);
 		
-		
-		Display.operation(processed.getCommand(), processed.getContent().get(0));
+		if(performCommand(processed.getCommand(),processed.getContent())){
+			return message.operation(processed.getCommand(), processed.getContent().get(0));	
+		}
+		else{
+			return message.error(input);
+		}
 	}
 	private boolean performCommand(Command_Type command,ArrayList<String> content){
+		Storage action;
 		switch(command){
 		case ADD_TASK:
-			//should call storage method
+			TaskDate date = content.get(1);
+			action.addTask(content.get(0), content.get(1), content.get(2), content.get(3));
 			return true;
 		case EDIT_TASK:
 			
