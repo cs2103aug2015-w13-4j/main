3e8eb11da7cf1e78d3fcb302faa86a246cd60520
shirlenequah <shirlenequah@yahoo.com.sg>
2015-10-06 18:50:38 +0800

    included parser

--- src/logic/Operation.java
+++ src/logic/Operation.java
@@ -6,36 +6,36 @@ import database.Storage;
 import utilities.CommandElements;
 import utilities.Command_Type;
 import utilities.TaskDate;
+import parser.CommandParser;
 import parser.ProcessInput;
 
 public class Operation {
 	public String processOperation(String input){
 		//get the commandElements from parser
 		Display message;
-		CommandElements processed = ProcessInput(input);
+		CommandParser parser;
+		CommandElements processed = parser.ProcessInput(input);
 		
-		if(performCommand(processed.getCommand(),processed.getContent())){
-			return message.operation(processed.getCommand(), processed.getContent().get(0));	
+		if(performCommand(processed.getType(),processed)){
+			return message.operation(processed.getType(), processed.getDescription());	
 		}
 		else{
 			return message.error(input);
 		}
 	}
-	private boolean performCommand(Command_Type command,ArrayList<String> content){
+	private boolean performCommand(Command_Type command,CommandElements content){
 		Storage action;
 		switch(command){
 		case ADD_TASK:
-<<<<<<< HEAD
-			TaskDate date = content.getDate();
-			
-			action.addTask(content.get(0), date, content.get(2), content.get(3));
-=======
-			action.addTask(content.get(0)+ content.get(1)+content.get(2)+content.get(3));
-			//action.addTask(content.get(0), content.get(1), content.get(2), content.get(3));
->>>>>>> origin/master
+			action.addTask(content.getDescription(), content.getDate(),content.getPriority());
+
+			//action.addTask(content.get(0)+ content.get(1)+content.get(2)+content.get(3));
 			return true;
 		case EDIT_TASK:
-			
+			//get the prev taskEvent
+			//replace the repective field
+			//edit
+			action.editTask(content.getObject(), prevTask, currTask)
 			return true;
 		}
 		return false;
