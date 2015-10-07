120350d3d52d4cbb83cbf2d7d1b423c9108e8e8a
shirlenequah <shirlenequah@yahoo.com.sg>
2015-10-07 10:52:07 +0800

    create new methods

--- src/logic/Launch.java
+++ src/logic/Launch.java
@@ -1,15 +1,20 @@
 package logic;
 
+import java.util.ArrayList;
+
 import database.Storage;
 import logic.Launch;
+import utilities.TaskEvent;
 
 public class Launch {
 	static Storage storage;
 	static Display display;
+	static Operation op;
 	
 	public static void createObjects(){
 		storage = new Storage();
 		display = new Display();
+		op = new Operation();
 	}
 	public void newFile(){
 		createObjects();
@@ -26,5 +31,11 @@ public class Launch {
 	}
 	public static  Display getDisplay(){
 		return display;
+	}
+	public ArrayList<TaskEvent> updateView(){
+		return display.defaultView();
+	}
+	public String command(String input){
+		return op.processOperation(input);
 	}
 }
