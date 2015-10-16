3d26d3d61b0f04df37335cc168188b78bd42fc00
shirlenequah <shirlenequah@yahoo.com.sg>
2015-10-16 17:32:05 +0800

    added search command

--- src/logic/Launch.java
+++ src/logic/Launch.java
@@ -10,31 +10,39 @@ public class Launch {
 	static Storage storage;
 	static Display display;
 	static Operation op;
-	
-	public static void createObjects(){
+	static Search search;
+	static ArrayList<TaskEvent> tasks;
+	private static void createObjects(){
 		storage = new Storage();
 		display = new Display();
 		op = new Operation();
+		search = new Search();
+		tasks = storage.load();
 	}
 	public void newFile(){
 		createObjects();
-		
 	}
 	public void loadFile(){
 		createObjects();
 	}
-	public void changeDirectory(String oldDirectory, String newDirectory){
-		
-	}
+	
+	/*getting obejcts*/
 	public static Storage getStorage(){
 		return storage;
 	}
 	public static  Display getDisplay(){
 		return display;
 	}
-	public ArrayList<TaskEvent> updateView(){
-		return display.defaultView();
+	public static Search getSearch(){
+		return search;
 	}
+	
+	/*UI main task view*/
+	public ArrayList<TaskEvent> updateView(){
+		tasks = display.taskView();
+		return tasks;
+	}
+	/*main method UI calls*/
 	public String command(String input){
 		return op.processOperation(input);
 	}
