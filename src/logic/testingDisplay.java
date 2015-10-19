473975c85021be46c96055a0ff739eff8a5c9399
shirlenequah <shirlenequah@yahoo.com.sg>
2015-10-19 16:36:37 +0800

    commands

--- src/logic/testingDisplay.java
+++ src/logic/testingDisplay.java
@@ -10,6 +10,9 @@ public class testingDisplay {
 	Display display = new Display();
 	
 	@Test
+	/**
+	 * testing to see if there is a correct output when command exist
+	 */
 	public void testOperation() {
 		Command_Type op = Command_Type.ADD_TASK;
 		String content = "new task";
@@ -17,6 +20,9 @@ public class testingDisplay {
 		String expected = "new task has been added sucessfully";
 		assertEquals(expected, actual);
 	}
+	/**
+	 * testing to see if the feddback is correct when error method is called
+	 */
 	public void testError(){
 		
 		String actual = display.error("invalid operation");
