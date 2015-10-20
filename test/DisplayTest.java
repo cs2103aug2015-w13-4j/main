187ae4ec4fd81efb3f560c0ad00f97d6dd5a3ba4
zhongwei <zhangzhongwei.sg@gmail.com>
2015-10-20 17:40:19 +0800

    have a test folder

---
+++ test/DisplayTest.java
@@ -0,0 +1,33 @@
+package logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import utilities.Command_Type;
+
+public class DisplayTest {
+	Display display = new Display();
+	
+	@Test
+	/**
+	 * testing to see if there is a correct output when command exist
+	 */
+	public void testOperation() {
+		Command_Type op = Command_Type.ADD_TASK;
+		String content = "new task";
+		String actual = display.operation(op,content);
+		String expected = "new task has been added sucessfully";
+		assertEquals(expected, actual);
+	}
+	/**
+	 * testing to see if the feddback is correct when error method is called
+	 */
+	public void testError(){
+		
+		String actual = display.error("invalid operation");
+		String expected = "invalid operation could not be performed";
+		assertEquals(expected,actual);
+	}
+
+}
