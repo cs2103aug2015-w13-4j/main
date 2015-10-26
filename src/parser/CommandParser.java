367d495f018e24b68ca6549d307db6d554d97de5
Felix Zhang Tianrui <felixatnus2013@gmail.com>
2015-10-26 09:07:47 +0800

    Update CommandParser.java

--- src/parser/CommandParser.java
+++ src/parser/CommandParser.java
@@ -1,6 +1,5 @@
 package parser;
 
-
 import utilities.Command_Field;
 import utilities.Command_Priority;
 import utilities.Command_Type;
@@ -12,48 +11,39 @@ public class CommandParser {
 	public static CommandElements ProcessInput(String command) {
 		Command_Type type;
 		String name;
-		TaskDate date;
+		TaskDate date[] = new TaskDate[2];
 		Command_Priority priority;
 		Command_Field field;
 		int object;
-		String parts[] = command.split(" ");
 		type = CommandSplitter.findType(command);
+		name = CommandSplitter.findName(command);
+		date = CommandSplitter.findDate(command);
+		priority = CommandSplitter.findPriority(command);
+		field = CommandSplitter.findField(command);
 		if (type == Command_Type.ADD_TASK) {
-			name = parts[1];
-			date = DateParser.dateDecoder(parts[2]);
-			priority = PriorityParser.PriorityDecoder(parts[3]);
 			return new CommandElements(type, name, date, priority);
 		} else if (type == Command_Type.EDIT_TASK) {
-			object = Integer.parseInt(parts[1]);
-			if (parts[2].equals("name")) {
-				field = Command_Field.NAME;
-				name = parts[3];
+			object = CommandSplitter.findObject(command);
+			if (field == Command_Field.NAME) {
 				return new CommandElements(type, object, field, name);
-			} else if (parts[2].equals("start_date")) {
-				field = Command_Field.START_DATE;
-				date = DateParser.dateDecoder(parts[3]);
-				return new CommandElements(type, object, field, date);
-			} else if (parts[2].equals("end_date")) {
-				field = Command_Field.END_DATE;
-				date = DateParser.dateDecoder(parts[3]);
-				return new CommandElements(type, object, field, date);
-			} else if (parts[2].equals("priority")) {
-				field = Command_Field.PRIORITY;
-				priority = PriorityParser.PriorityDecoder(parts[3]);
+			} else if (field == Command_Field.START_DATE) {
+				return new CommandElements(type, object, field, date[0]);
+			} else if (field == Command_Field.END_DATE) {
+				return new CommandElements(type, object, field, date[0]);
+			} else if (field == Command_Field.PRIORITY) {
 				return new CommandElements(type, object, field, priority);
 			} else {
 				return null;
 			}
 		} else if (type == Command_Type.DELETE_TASK) {
-			object = Integer.parseInt(parts[1]);
+			object = CommandSplitter.findObject(command);
 			return new CommandElements(type, object);
 		} else if (type == Command_Type.FINISH_TASK) {
-			object = Integer.parseInt(parts[1]);
+			object = CommandSplitter.findObject(command);
 			return new CommandElements(type, object);
 		} else if (type == Command_Type.UNDO) {
 			return new CommandElements(type);
 		} else if (type == Command_Type.DIRECTORY) {
-			name = parts[1];
 			return new CommandElements(type, name);
 		} else {
 			return null;
