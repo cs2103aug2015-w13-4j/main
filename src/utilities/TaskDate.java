7375b70d71559d284a4f0f0505a969528ddcf07a
Felix Zhang Tianrui <felixatnus2013@gmail.com>
2015-10-26 09:09:30 +0800

    Update TaskDate.java

--- src/utilities/TaskDate.java
+++ src/utilities/TaskDate.java
@@ -2,70 +2,49 @@ package utilities;
 
 import java.util.ArrayList;
 
-public class TaskDate {
-
+public class TaskDate implements Comparable<TaskDate>{
+	
 	String myDate;
 	//static int myYear;
 	int myYear, myDay, myMonth;
-
+	
 	// constructor 
 	public TaskDate(int year, int month, int day) {
 		myYear = year;
 		myMonth = month;
 		myDay = day;
 	}
-
-	public TaskDate(String date) {
-		if (date.equals("null")) {
-			myYear = 0;
-			myMonth = 0;
-			myDay = 0;
-			return;
-		}
-		String[] split = date.split("/");
-		myDay = Integer.parseInt(split[0]);
-		myMonth = Integer.parseInt(split[1]);
-		myYear = Integer.parseInt(split[2]);
-	}
-
-	//no date
-	//represent null
+	
 	public TaskDate() {
-		myYear = -1;
-		myMonth = -1;
-		myDay = -1;
-	}	
-
+		myYear = 0;
+		myMonth = 0;
+		myDay = 0;
+	}
+	
 	public void setYear(int year) {
 		myYear = year;
 	}
-
+	
 	public void setMonth(int month) {
 		myMonth = month;
 	}
-
+	
 	public void setDay(int day) {
 		myDay = day;
 	}
-
+	
 	public int getYear() {
 		return myYear;
 	}
-
+	
 	public int getMonth() {
 		return myMonth;
 	}
-
+	
 	public int getDay() {
 		return myDay;
 	}
-	public String toString(){
-		if(myYear==-1){
-			return "null";
-		}
-		return myDay + "/" + myMonth +"/" + myYear;
-	}
-
+	
 	public String printDate() {
 		if (myDay < 10 && myMonth < 10) {
 			return "0" + Integer.toString(myDay) + "/" + "0" + Integer.toString(myMonth) + "/" + Integer.toString(myYear);
@@ -77,7 +56,7 @@ public class TaskDate {
 			return Integer.toString(myDay) + "/" + Integer.toString(myMonth) + "/" + Integer.toString(myYear);
 		}
 	}
-
+	
 	public ArrayList<String> getMultipleDate() {
 		ArrayList<String> date_formats = new ArrayList<String>();
 		String format_1 = String.valueOf(myDay)+String.valueOf(myMonth)+String.valueOf(myYear);
@@ -122,7 +101,7 @@ public class TaskDate {
 		date_formats.add(format_20);
 		return date_formats;
 	}
-
+	
 	private String formatChange(String date) {
 		if (date.length() == 2) {
 			return date;
@@ -132,6 +111,17 @@ public class TaskDate {
 			return "0" + date;
 		} else {
 			return date;
+		}
+	}
+
+	@Override
+	public int compareTo(TaskDate arg0) {
+		int thisVal = this.myYear * 10000 + this.myMonth * 100 + this.myDay;
+		int thatVal = arg0.getYear() * 10000 + arg0.getMonth() * 100 + arg0.getDay();
+		if (thisVal > thatVal) {
+			return 1;
+		} else {
+			return 0;
 		}
 	}
 }
