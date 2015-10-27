1d451874479c47642669896dccd6e268d43f8deb
shirlenequah <shirlenequah@yahoo.com.sg>
2015-10-27 13:16:47 +0800

    remove error

--- src/utilities/TaskEventTest.java
+++ src/utilities/TaskEventTest.java
@@ -1,8 +1,8 @@
 package utilities;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
-import org.junit.Before;
+//import org.junit.Before;
 import org.junit.Test;
 
 public class TaskEventTest {
@@ -10,15 +10,15 @@ public class TaskEventTest {
 
 	@Test
 	public void testCreate() {
-		TaskEvent task = new TaskEvent(1 , "Hello" , new TaskDate(10,10,2015) , new TaskDate(10,15,2015) , "high");
+		TaskEvent task = new TaskEvent(1 , "Hello" , new TaskDate(10,10,2015) , new TaskDate(10,15,2015) , Command_Priority.HIGH);
 		assert task != null;
-		TaskEvent taskTwo = new TaskEvent(1, "Bye" , new TaskDate(10,10,2015) , new TaskDate(10,15,2015) , 1);
+		TaskEvent taskTwo = new TaskEvent(1, "Bye" , new TaskDate(10,10,2015) , new TaskDate(10,15,2015) , Command_Priority.HIGH);
 		assert taskTwo != null;
 	}
 	@Test
 	public void testGetter() {
-		TaskEvent task = new TaskEvent(1 , "Hello" , new TaskDate(10,10,2015) , new TaskDate(10,15,2015) , "high");
-		TaskEvent taskTwo = new TaskEvent(1, "Bye" , new TaskDate(10,10,2015) , new TaskDate(10,15,2015) , 1);
+		TaskEvent task = new TaskEvent(1 , "Hello" , new TaskDate(10,10,2015) , new TaskDate(10,15,2015) , Command_Priority.HIGH);
+		TaskEvent taskTwo = new TaskEvent(1, "Bye" , new TaskDate(10,10,2015) , new TaskDate(10,15,2015) , Command_Priority.HIGH);
 		
 		assert task.getStartDate().equals(new TaskDate(10,10,2015) );
 		assert taskTwo.getPriority().equals("high");		
