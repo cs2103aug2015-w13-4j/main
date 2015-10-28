d9bccdefee95918702aaf630e9cc7a233fbe802f
Felix Zhang Tianrui <felixatnus2013@gmail.com>
2015-10-28 12:58:42 +0800

    updated formats

--- src/parser/DateParser.java
+++ src/parser/DateParser.java
@@ -11,7 +11,7 @@ public class DateParser {
 		int year, month, day;
 		int thisDate[] = new int[3];
 		thisDate = getCurrentDate();
-		
+
 		// support format 03/09/2015
 		if (dateStr.length() == 10) {
 			String parts[] = dateStr.split("/");
@@ -24,7 +24,32 @@ public class DateParser {
 				}
 			}
 		}
-		
+		// support format 03/09/2015
+		if (dateStr.length() == 9) {
+			String parts[] = dateStr.split("/");
+			if (parts.length == 3){
+				day = Integer.parseInt(parts[0]);
+				month = Integer.parseInt(parts[1]);
+				year = Integer.parseInt(parts[2]);
+				if (checkDate(year, month, day) == 0) {
+					return new TaskDate(year, month, day);
+				}
+			}
+		}
+
+		// support format 03/09/2015
+		if (dateStr.length() == 8) {
+			String parts[] = dateStr.split("/");
+			if (parts.length == 3){
+				day = Integer.parseInt(parts[0]);
+				month = Integer.parseInt(parts[1]);
+				year = Integer.parseInt(parts[2]);
+				if (checkDate(year, month, day) == 0) {
+					return new TaskDate(year, month, day);
+				}
+			}
+		}
+
 		// support format 2015/09/03
 		if (dateStr.length() == 10) {
 			String parts[] = dateStr.split("/");
@@ -37,9 +62,70 @@ public class DateParser {
 				}
 			}
 		}
-		
+
+		// support format 2015/09/03
+		if (dateStr.length() == 10) {
+			String parts[] = dateStr.split("/");
+			if (parts.length == 3){
+				day = Integer.parseInt(parts[2]);
+				month = Integer.parseInt(parts[1]);
+				year = Integer.parseInt(parts[0]);
+				if (checkDate(year, month, day) == 0) {
+					return new TaskDate(year, month, day);
+				}
+			}
+		}
+		// support format 2015/09/03
+		if (dateStr.length() == 9) {
+			String parts[] = dateStr.split("/");
+			if (parts.length == 3){
+				day = Integer.parseInt(parts[2]);
+				month = Integer.parseInt(parts[1]);
+				year = Integer.parseInt(parts[0]);
+				if (checkDate(year, month, day) == 0) {
+					return new TaskDate(year, month, day);
+				}
+			}
+		}
+		// support format 2015/09/03
+		if (dateStr.length() == 8) {
+			String parts[] = dateStr.split("/");
+			if (parts.length == 3){
+				day = Integer.parseInt(parts[2]);
+				month = Integer.parseInt(parts[1]);
+				year = Integer.parseInt(parts[0]);
+				if (checkDate(year, month, day) == 0) {
+					return new TaskDate(year, month, day);
+				}
+			}
+		}
 		// support format 03/09/15
 		if (dateStr.length() == 8) {
+			String parts[] = dateStr.split("/");
+			if (parts.length == 3){
+				day = Integer.parseInt(parts[0]);
+				month = Integer.parseInt(parts[1]);
+				year = Integer.parseInt(parts[2]) + 2000;
+				if (checkDate(year, month, day) == 0) {
+					return new TaskDate(year, month, day);
+				}
+			}
+		}
+
+		// support format 03/09/15
+		if (dateStr.length() == 7) {
+			String parts[] = dateStr.split("/");
+			if (parts.length == 3){
+				day = Integer.parseInt(parts[0]);
+				month = Integer.parseInt(parts[1]);
+				year = Integer.parseInt(parts[2]) + 2000;
+				if (checkDate(year, month, day) == 0) {
+					return new TaskDate(year, month, day);
+				}
+			}
+		}
+		// support format 03/09/15
+		if (dateStr.length() == 6) {
 			String parts[] = dateStr.split("/");
 			if (parts.length == 3){
 				day = Integer.parseInt(parts[0]);
@@ -64,8 +150,60 @@ public class DateParser {
 			}
 		}
 
+		// support format 15/09/03
+		if (dateStr.length() == 7) {
+			String parts[] = dateStr.split("/");
+			if (parts.length == 3){
+				day = Integer.parseInt(parts[2]);
+				month = Integer.parseInt(parts[1]);
+				year = Integer.parseInt(parts[0]) + 2000;
+				if (checkDate(year, month, day) == 0) {
+					return new TaskDate(year, month, day);
+				}
+			}
+		}
+
+		// support format 15/09/03
+		if (dateStr.length() == 6) {
+			String parts[] = dateStr.split("/");
+			if (parts.length == 3){
+				day = Integer.parseInt(parts[2]);
+				month = Integer.parseInt(parts[1]);
+				year = Integer.parseInt(parts[0]) + 2000;
+				if (checkDate(year, month, day) == 0) {
+					return new TaskDate(year, month, day);
+				}
+			}
+		}
+
 		// support format 09/03
 		if (dateStr.length() == 5) {
+			String parts[] = dateStr.split("/");
+			if (parts.length == 2){
+				year = thisDate[0];
+				month = Integer.parseInt(parts[0]);
+				day = Integer.parseInt(parts[1]);
+				if (checkDate(year, month, day) == 0) {
+					return new TaskDate(year, month, day);
+				}
+			}
+		}
+
+		// support format 09/03
+		if (dateStr.length() == 4) {
+			String parts[] = dateStr.split("/");
+			if (parts.length == 2){
+				year = thisDate[0];
+				month = Integer.parseInt(parts[0]);
+				day = Integer.parseInt(parts[1]);
+				if (checkDate(year, month, day) == 0) {
+					return new TaskDate(year, month, day);
+				}
+			}
+		}
+
+		// support format 09/03
+		if (dateStr.length() == 3) {
 			String parts[] = dateStr.split("/");
 			if (parts.length == 2){
 				year = thisDate[0];
@@ -88,12 +226,51 @@ public class DateParser {
 				}
 			}
 		}
-		
+
+		// support format 03/09
+		if (dateStr.length() == 5) {
+			String parts[] = dateStr.split("/");
+			if (parts.length == 2){
+				year = thisDate[0];
+				month = Integer.parseInt(parts[1]);
+				day = Integer.parseInt(parts[0]);
+				if (checkDate(year, month, day) == 0) {
+					return new TaskDate(year, month, day);
+				}
+			}
+		}
+
+		// support format 03/09
+		if (dateStr.length() == 4) {
+			String parts[] = dateStr.split("/");
+			if (parts.length == 2){
+				year = thisDate[0];
+				month = Integer.parseInt(parts[1]);
+				day = Integer.parseInt(parts[0]);
+				if (checkDate(year, month, day) == 0) {
+					return new TaskDate(year, month, day);
+				}
+			}
+		}
+
+		// support format 03/09
+		if (dateStr.length() == 3) {
+			String parts[] = dateStr.split("/");
+			if (parts.length == 2){
+				year = thisDate[0];
+				month = Integer.parseInt(parts[1]);
+				day = Integer.parseInt(parts[0]);
+				if (checkDate(year, month, day) == 0) {
+					return new TaskDate(year, month, day);
+				}
+			}
+		}
+
 		return new TaskDate(0, 0, 0);
 	}
-	
+
 	public static int checkDate(int year, int month, int day) {
-		
+
 		if (month < 1 || month > 12) {
 			return -1;
 		}
@@ -120,15 +297,15 @@ public class DateParser {
 		}
 		return 0;
 	}
-	
+
 	private static int[] getCurrentDate() {
 		Date date = new Date(); // your date
-	    Calendar cal = Calendar.getInstance();
-	    cal.setTime(date);
-	    int thisYear = cal.get(Calendar.YEAR);
-	    int thisMonth = cal.get(Calendar.MONTH) + 1;
-	    int thisDay = cal.get(Calendar.DAY_OF_MONTH);
-	    int currentDate[] = {thisYear, thisMonth, thisDay};
-	    return currentDate;
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		int thisYear = cal.get(Calendar.YEAR);
+		int thisMonth = cal.get(Calendar.MONTH) + 1;
+		int thisDay = cal.get(Calendar.DAY_OF_MONTH);
+		int currentDate[] = {thisYear, thisMonth, thisDay};
+		return currentDate;
 	}
 }
