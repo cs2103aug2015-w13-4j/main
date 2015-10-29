1d2c92823ecbc0c554d7408db18d1e9a1f02ee4e
shirlenequah <shirlenequah@yahoo.com.sg>
2015-10-29 21:24:37 +0800

    included exception

--- src/parser/CommandParser.java
+++ src/parser/CommandParser.java
@@ -5,10 +5,14 @@ import utilities.Command_Priority;
 import utilities.Command_Type;
 import utilities.CommandElements;
 import utilities.TaskDate;
+import utilities.Exceptions.CommandNotFound;
+import utilities.Exceptions.EditFieldNotFound;
 
 public class CommandParser {
 
-	public static CommandElements ProcessInput(String command) {
+	public static CommandElements ProcessInput(String command) throws Exception {
+		CommandNotFound command_exception = new CommandNotFound("command not found");
+		EditFieldNotFound edit_exception = new EditFieldNotFound("edit field not found");
 		Command_Type type;
 		String name;
 		TaskDate date[] = new TaskDate[2];
@@ -33,7 +37,7 @@ public class CommandParser {
 			} else if (field == Command_Field.PRIORITY) {
 				return new CommandElements(type, object, field, priority);
 			} else {
-				return null;
+				throw edit_exception;
 			}
 		} else if (type == Command_Type.DELETE_TASK) {
 			object = CommandSplitter.findObject(command);
@@ -51,9 +55,7 @@ public class CommandParser {
 		} else if (type == Command_Type.DIRECTORY) {
 			return new CommandElements(type, name);
 		} else {
-			return null;
+			throw command_exception;
 		}
 	}
-	
-
 }
