54ed75d9fc0e55bf09053f7444f8711f8f4562bc
shirlenequah <shirlenequah@yahoo.com.sg>
2015-11-03 16:43:26 +0800

    redo

--- src/logic/Redo.java
+++ src/logic/Redo.java
@@ -12,59 +12,59 @@ public class Redo {
 	StorageImp storage;
 
 	public Redo(){
-		logger = Logger.getLogger("Undo");
+		logger = Logger.getLogger("redo");
 		storage = Launch.getStorage();
 	}
-	public boolean UndoTask(CommandElements content){
+	public boolean redoTask(CommandElements content){
 		
-		logger.log(Level.INFO, "starting to undo");
+		logger.log(Level.INFO, "starting to redo");
 		Command_Type type = content.getType();
 		switch(type){
 		case ADD_TASK:
-			return UndoAdd(content);
+			return redoAdd(content);
 		case DELETE_TASK:
-			return UndoDelete(content);
+			return redoDelete(content);
 		case EDIT_TASK:
-			return UndoEdit(content);
+			return redoEdit(content);
 		case FINISH_TASK:
-			return UndoComplete(content);
+			return redoComplete(content);
 		case DIRECTORY:
-			return UndoDirectory(content);
+			return redoDirectory(content);
 		case FLAG_TASK:
-			return UndoFlag(content);
+			return redoFlag(content);
 		case UNFLAG_TASK:
-			return UndoUnflag(content);
+			return redoUnflag(content);
 		default :
 			return false;
 		}
 	}
-	private boolean UndoAdd(CommandElements content){
-		logger.log(Level.INFO, "undo add");
-		return storage.deleteTaskByID(content.getID());
-	}
-	private boolean UndoDelete(CommandElements content){
-		logger.log(Level.INFO, "undo delete");
+	private boolean redoAdd(CommandElements content){
+		logger.log(Level.INFO, "redo add");
 		return storage.undoDeleteTaskByID(content.getID());
 	}
-	private boolean UndoEdit(CommandElements content){
-		logger.log(Level.INFO, "undo edit");
+	private boolean redoDelete(CommandElements content){
+		logger.log(Level.INFO, "redo delete");
+		return storage.deleteTaskByID(content.getID());
+	}
+	private boolean redoEdit(CommandElements content){
+		logger.log(Level.INFO, "redo edit");
 		return storage.editTask(content.getID(), content.getField(), content.getName());
 	}
-	private boolean UndoComplete(CommandElements content){
-		logger.log(Level.INFO, "undo Complete");
-		return storage.markTaskAsUndone(content.getID());
+	private boolean redoComplete(CommandElements content){
+		logger.log(Level.INFO, "redo Complete");
+		return storage.markTaskAsDone(content.getID());
 	}
-	private boolean UndoDirectory(CommandElements content){
+	private boolean redoDirectory(CommandElements content){
 		//not implemented
 		return false;
 	}
-	private boolean UndoFlag(CommandElements content){
-		logger.log(Level.INFO, "undo flag");
-		return storage.unflagTask(content.getID());
-	}
-	private boolean UndoUnflag(CommandElements content){
-		logger.log(Level.INFO,"undo unflag");
+	private boolean redoFlag(CommandElements content){
+		logger.log(Level.INFO, "redo flag");
 		return storage.flagTask(content.getID());
+	}
+	private boolean redoUnflag(CommandElements content){
+		logger.log(Level.INFO,"undo unflag");
+		return storage.unflagTask(content.getID());
 	}
 	
 }
