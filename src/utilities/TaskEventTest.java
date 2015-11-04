00e5e1f2b055277d8cd531d4ad84f400150228c4
zhongwei <zhangzhongwei.sg@gmail.com>
2015-11-04 21:22:39 +0800

    test case for new taskevent

--- src/utilities/TaskEventTest.java
+++ src/utilities/TaskEventTest.java
@@ -10,17 +10,17 @@ public class TaskEventTest {
 
 	@Test
 	public void testCreate() {
-		TaskEvent task = new TaskEvent(1 , "Hello" , new TaskDate(10,10,2015) , new TaskDate(10,15,2015) , Command_Priority.FLAG);
+		TaskEvent task = new TaskEvent(1 , "Hello" , new TaskDate(10,10,2015), new TaskTime(10, 10), new TaskDate(10,15,2015), new TaskTime(13, 13), Command_Priority.FLAG);
 		assert task != null;
-		TaskEvent taskTwo = new TaskEvent(1, "Bye" , new TaskDate(10,10,2015) , new TaskDate(10,15,2015) , Command_Priority.FLAG);
+		TaskEvent taskTwo = new TaskEvent(1, "Bye" , new TaskDate(10,10,2015) , new TaskTime(12, 12), new TaskDate(10,15,2015), new TaskTime(14, 14), Command_Priority.FLAG);
 		assert taskTwo != null;
-		TaskEvent task3 = new TaskEvent(1, "task3", null, null, Command_Priority.UNFLAG);
+		TaskEvent task3 = new TaskEvent(1, "task3", null, null, null, null, Command_Priority.UNFLAG);
 		System.out.println("task3:" + task3.toString());
 	}
 	@Test
 	public void testGetter() {
-		TaskEvent task = new TaskEvent(1 , "Hello" , new TaskDate(10,10,2015) , new TaskDate(10,15,2015) , Command_Priority.FLAG);
-		TaskEvent taskTwo = new TaskEvent(1, "Bye" , new TaskDate(10,10,2015) , new TaskDate(10,15,2015) , Command_Priority.FLAG);
+		TaskEvent task = new TaskEvent(1, "Hello", new TaskDate(10,10,2015), new TaskTime(13, 13), new TaskDate(10,15,2015), new TaskTime(14, 14), Command_Priority.FLAG);
+		TaskEvent taskTwo = new TaskEvent(1, "Bye", new TaskDate(10,10,2015), new TaskTime(15, 15), new TaskDate(10,15,2015), new TaskTime(16, 16), Command_Priority.FLAG);
 		
 		assert task.getStartDate().equals(new TaskDate(10,10,2015) );
 		assert taskTwo.getPriority().equals("high");		
