acab9e47d2b7a1c911eb473d5206a11eaa3d94ef
zhongwei <zhangzhongwei.sg@gmail.com>
2015-11-08 11:31:29 +0800

    major cleanup of database package with javadoc

--- src/database/FileUtils.java
+++ src/database/FileUtils.java
@@ -3,7 +3,16 @@ package database;
 import java.io.*;
 
 // @@author A0130503B
+
+/**
+ * A helper class to do File I/O
+ */
 public class FileUtils {
+	/**
+	 * Checks if a file name is valid by creating a File and using {@link File#getCanonicalPath()}
+	 * @param filename the file name to be checked
+	 * @return boolean if there is no IOException
+	 */
 	public static boolean isFilenameValid(String filename) {
 		File file = new File(filename);
 		try {
@@ -14,10 +23,21 @@ public class FileUtils {
 		return true;
 	}
 
+	/**
+	 * Checks if the file exists and is a directory
+	 * @param file the file to be checked
+	 * @return true if file exists and is a directory
+	 */
 	public static boolean isValidDirectory(File file) {
 		return file.exists() && file.isDirectory();
 	}
 
+	/**
+	 * Get the first line of a file
+	 * @param filename name of the file
+	 * @return the first line as a String
+	 * @throws IOException
+	 */
 	public static String getFirstLine(String filename)
 			throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
@@ -26,6 +46,12 @@ public class FileUtils {
 		return string;
 	}
 
+	/**
+	 * Move the a file from src location to dst location, and delete the src file after moving
+	 * @param src source path
+	 * @param dst destination path
+	 * @throws IOException
+	 */
 	public static void moveFile(String src, String dst)
 			throws IOException {
 		File srcFile = new File(src);
@@ -42,6 +68,13 @@ public class FileUtils {
 		bw.close();
 	}
 
+	/**
+	 * Count the lines in a file
+	 * @param dir the file to be counted
+	 * @return the line count number
+	 * @throws FileNotFoundException if file is not found
+	 * @throws IOException
+	 */
 	public static int getLinesCount(String dir)
 			throws FileNotFoundException, IOException {
 		int counter = 0;
@@ -55,6 +88,19 @@ public class FileUtils {
 			throw e;
 		} catch (IOException e) {
 			throw e;
+		}
+	}
+
+	/**
+	 * Append a file name after dir if it is only a directory name.
+	 * @param dir path
+	 * @return a String of the updated path name
+	 */
+	public static String addDefFileNameToDir(String dir) {
+		if (dir.endsWith(StorageImp.DIV)) {
+			return dir + StorageImp.DEFAULT_SAVE_DIR;
+		} else {
+			return dir + StorageImp.DIV + StorageImp.DEFAULT_SAVE_DIR;
 		}
 	}
 }
---
+++ src/database/SelectScope.java
@@ -0,0 +1,10 @@
+package database;
+
+// @@author A0130503B
+
+/**
+ * Enum to indicate which tasks to be selected
+ */
+public enum SelectScope {
+	DELETED, COMPLETED, FRESH
+}
